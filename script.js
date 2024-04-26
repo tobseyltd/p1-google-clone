f1fb2dd0f0626be3f9e229afdb25f4073fda5dc2
chris-loves-frontend <cseyboth@mailbox.org>
2024-04-26 09:43:18 +0200

    JS search suggestions

--- script.js
+++ script.js
@@ -1,2 +1,46 @@
 // @ts-nocheck
-// Alle Formular Elemente aus dem DOM ziehen
+
+window.addEventListener('DOMContentLoaded', () => {
+    // Alle Elemente aus dem DOM ziehen
+    const searchInput = document.getElementById('searchInput');
+    const submitButton = document.getElementById('submitButton');
+    const suggestionSection = document.getElementById('suggestionSection');
+    const searchSection = document.querySelector('search');
+
+    const suggestResults = ['Javascript', 'Javascript lernen', 'Javascript array'];
+    let searchInputValue = '';
+
+    searchInput.addEventListener('input', ({ target: { value } }) => {
+        searchInputValue = value.toLowerCase();
+        suggestionSection.innerHTML = '';
+
+        searchSection.style.borderRadius = '20px 20px 0 0';
+        searchSection.style.background = '#303134';
+        searchSection.style.border = 'none';
+        suggestionSection.style.display = 'flex';
+
+        if (!searchInputValue) {
+            suggestionSection.style.display = 'none';
+            searchSection.style.borderRadius = '50px';
+            searchSection.style.border = '1px solid #5f6368';
+            searchSection.style.background = 'transparent';
+        }
+
+        suggestResults.forEach((suggest) => {
+            if (suggest.toLowerCase().includes(searchInputValue)) {
+                const searchSuggestion = document.createElement('search-suggestion');
+
+                searchSuggestion.innerHTML = `
+                    <img src="./assets/search.svg" alt width="17" height="17" /> ${suggest}
+                `;
+
+                suggestionSection.appendChild(searchSuggestion);
+            } else {
+                suggestionSection.style.display = 'none';
+                searchSection.style.borderRadius = '50px';
+                searchSection.style.border = '1px solid #5f6368';
+                searchSection.style.background = 'transparent';
+            }
+        });
+    });
+});
